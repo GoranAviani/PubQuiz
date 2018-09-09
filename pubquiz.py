a203a561066d951061393459f79bb146fd6746a3
GoranAviani <goranaviani@gmail.com>
2018-09-09 09:42:05 +0200

    main menu built

---
+++ pubquiz.py
@@ -0,0 +1,35 @@
+
+def main_menu():
+    userCommand = input("\n\n       ***Wellcome to Pub Quiz MAIN MENU!***\n\n-------------------------------------------------------\n* If you want to add new questions to enter [N]\n* IF you want to list all questions and answers enter [L]"
+                        "\n* If you want to start the new game enter [G]\n* Enter [X] to exit game*\n-------------------------------------------------------"
+                        "\n\nYour command: ")
+    return userCommand
+
+
+
+def main():
+
+    while True:
+        userCommand = main_menu()
+
+        #add new questions
+        if userCommand.upper() == "N":
+            print("adding new questions")
+
+        elif userCommand.upper() == "L":
+            print("listing all questions")
+
+        elif userCommand.upper() == "X":
+            print("Good bye!")
+            break
+
+            # start game
+        elif userCommand.upper() == "G":
+            print("game has started")
+
+        else:
+            print("I don't understand that command.")
+
+
+if __name__ == '__main__':
+    main()
