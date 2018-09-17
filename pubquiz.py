42708e61f7a1c6dcf652031184d7a8be479cf748
GoranAviani <goranaviani@gmail.com>
2018-09-17 14:36:31 +0200

    processing quiz questions

--- pubquiz.py
+++ pubquiz.py
@@ -11,6 +11,7 @@ def load_from_file():
     quizQuestions = [
         {
             "id": 1,
+            "category": 1,
             "Question": "What is the name of capital of Sweden?",
             "a": "Oslo",
             "b": "Stockholm",
@@ -20,30 +21,87 @@ def load_from_file():
         },
         {
             "id": 2,
+            "category": 1,
             "Question": "test?",
             "a": "test1",
             "b": "test2",
             "c": "test3",
             "d": "test4",
             "answer": "b"
+        },
+        {
+            "id": 3,
+            "category": 2,
+            "Question": "test2222?",
+            "a": "test1",
+            "b": "test2",
+            "c": "test3",
+            "d": "test4",
+            "answer": "d"
+        },
+        {
+            "id": 4,
+            "category": 3,
+            "Question": "test3333?",
+            "a": "test1",
+            "b": "test2",
+            "c": "test3",
+            "d": "test4",
+            "answer": "c"
         }
     ]
     return quizQuestions
 
 
-def process_questions(quizQuestions):
+def process_questions(quizQuestions, go_noOfQuestions, go_noOfCategory):
     #idea is to process questions by number of needed questions for this quiz,
     #  and taht questions dont repeat in a single quiz (thats why id).
-    #TODO categorise questions
+    #TODO categorise questions, there can be more categories for 1 quiz
+
+    gameQuestionsCounter = 0
+    gameQuestionsPreselectedIDs = []
+    gameQuestionsPreselected = []
+    gameQuestionsPreselectedFinal = []
     print("quiz questions are being processed..")
-    pass
+
+    #first get all questions of selected categories
+    for question in quizQuestions:
+        if question["category"] in go_noOfCategory:
+            gameQuestionsPreselected.append(question)
+            gameQuestionsPreselectedIDs.append(question["id"])
+
+    #if there are no questions in selected categories
+    if len(gameQuestionsPreselected) == 0:
+        process_questonsMessage = "There were no questions in selected categories!"
+        process_questonsStatus = "NO"
+    else:
+        for questionPreselected in gameQuestionsPreselected:
+            # if num of selected questions is num of asked questions then end
+            if gameQuestionsCounter == go_noOfQuestions:
+                process_questonsMessage = "Questions succesfully selected!"
+                process_questonsStatus = "YES"
+                break
+            else:
+                if questionPreselected["id"] not in gameQuestionsPreselectedIDs: #make sure we dont repeat questions
+                    gameQuestionsPreselectedFinal.append(question)
+                    gameQuestionsCounter += 1
+        print(gameQuestionsPreselectedFinal)
+
+    return (process_questonsMessage, process_questonsStatus, gameQuestionsPreselectedFinal  )
 
 def choose_game_options(quizQuestions):
     #TODO categorese questions
     #option 1: how many guestions
     #option 2: what categories
-    process_questions(quizQuestions)
+    print("Game options. 2 questions. category 1. SKIP.")
+    go_noOfQuestions = 2
+    go_noOfCategory = [1, 2]
 
+    process_questonsMessage, process_questonsStatus, gameQuestionsPreselectedFinal = process_questions(quizQuestions, go_noOfQuestions, go_noOfCategory)
+
+    print(process_questonsMessage)
+    print(process_questonsStatus)
+    print(gameQuestionsPreselectedFinal)
 
 def main():
     quizQuestions = (load_from_file())
